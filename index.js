13bb09b9ce698ef9e46ea2dc52fe5f9660591880
Austin Holeman <austinholeman@gmail.com>
2022-03-01 14:27:19 -0800

    commit tree 1d3d70579787d8bd4fe1795e055f0d71ee22a178

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+    cats;
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+    cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    cats;
+}
+
+function appendCat() {
+    let copyCat = [...cats, "Broom"];
+    return copyCat
+
+}
+
+function prependCat() {
+    let copyCat = ["Arnold", ...cats];
+    return copyCat
+
+}
+
+function removeLastCat() {
+    let caaat = cats.slice(0, cats.length-1);
+    return caaat;
+}
+
+function removeFirstCat() {
+    let caaat = cats.slice(1);
+    return caaat;
+}
